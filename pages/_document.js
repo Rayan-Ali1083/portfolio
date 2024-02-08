0dd2209bc25bd83d3dd26c5ae70dd1f12da863a3
Rayan Ali <k201083@nu.edu.pk>
2024-02-08 14:03:35 +0500

    adding meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { Analytics } from '@vercel/analytics/react';
 
-
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <Analytics />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Open Graph meta tags */}
+          <meta property="og:title" content="Data Scientist" />
+          <meta property="og:description" content="I hunt patterns in Data" />
+          <meta property="og:image" content="https://photos.google.com/u/1/photo/AF1QipMz5vWIJyKpDf2xEVd9xfxthRN7oIVd2tll_0hc" />
+          <meta property="og:url" content="https://m-rayan.vercel.app/" />
+        </Head>
+        <body>
+          <Main />
+          <Analytics />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
