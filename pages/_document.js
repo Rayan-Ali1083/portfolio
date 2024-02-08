bb9c0f32cecddf60b8b731c11c71a715cd9f812a
Rayan Ali <k201083@nu.edu.pk>
2024-02-08 14:15:44 +0500

    adding meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,24 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* Open Graph meta tags */}
-          <meta name="title" content="Data Scientist" />
+
+          <title>Data Scientist</title>
           <meta name="description" content="I hunt patterns in Data" />
-          <meta name="image" content="https://photos.google.com/u/1/photo/AF1QipMz5vWIJyKpDf2xEVd9xfxthRN7oIVd2tll_0hc" />
-          <meta name="url" content="https://m-rayan.vercel.app/" />
+
+          <meta property="og:url" content="https://m-rayan.vercel.app/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Data Scientist" />
+          <meta property="og:description" content="I hunt patterns in Data" />
+          <meta property="og:image" content="https://opengraph.b-cdn.net/production/documents/ab6c17c4-147e-4845-84b8-3e56272df748.png?token=oot2n40rNAzRSWex4iflkogXwxFfdPSeHx70eJAKF44&height=515&width=400&expires=33243383673" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="m-rayan.vercel.app" />
+          <meta property="twitter:url" content="https://m-rayan.vercel.app/" />
+          <meta name="twitter:title" content="Data Scientist" />
+          <meta name="twitter:description" content="I hunt patterns in Data" />
+          <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/documents/ab6c17c4-147e-4845-84b8-3e56272df748.png?token=oot2n40rNAzRSWex4iflkogXwxFfdPSeHx70eJAKF44&height=515&width=400&expires=33243383673" />
+
+
         </Head>
         <body>
           <Main />
